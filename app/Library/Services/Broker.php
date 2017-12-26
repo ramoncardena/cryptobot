59c57657a05c32f7c0b27c296942ab165b4c6323
Ramon Cardena <info@ramoncardena.com>
2017-12-26 10:47:05 +0100

    Fix: Keep requeueing jobs if exception.

--- app/Library/Services/Broker.php
+++ app/Library/Services/Broker.php
@@ -81,6 +81,7 @@ class Broker
     		switch (strtolower($this->exchange->name)) {
     			case 'bittrex':
     				$exchangeResponse = Bittrex::getTicker($market);
+                    Log::critical("[Broker] getTicker: " . var_dump($exchangeResponse));
 
     				if ($exchangeResponse->success == true) {
 
@@ -89,7 +90,7 @@ class Broker
 				        $response->message="";
 				        $response->result = new \stdClass();
                         // LOG: Exception trying to show trades
-                        Log::critical("[Broker] getTicker: " . var_dump($exchangeResponse->result));
+                       
 
 				        $response->result->Bid = $exchangeResponse->result->Bid;
 				        $response->result->Ask = $exchangeResponse->result->Ask;
